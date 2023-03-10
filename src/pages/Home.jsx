da81c140d784b32d1a455fa9a3709cb71a4745e4
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2023-03-09 20:09:44 -0800

    made some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,1 @@
-import React from "react";
-import { About } from "../components/Home/About";
-import { Nav } from "../components/Home/navbar/Nav";
-import { Services } from "../components/Home/services/Services";
-import { Header } from "../components/Home/navbar/Header";
 
-export const Home = () => {
-  return (
-    <div>
-      <Nav />
-      <Services />
-      <About />
-    </div>
-  );
-};
