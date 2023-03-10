5e4e39fa7868974694e13e86c124d3f942e3b4e1
Steve Ndicunguye <ndicunguyesteve4@gmail.com>
2023-03-09 20:12:27 -0800

    made some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,17 @@ import { Subscribe } from '../components/Home/Subscribe/Subscribe';
 import { Partners } from '../components/Home/Partners/Partners';
 import { Count } from '../components/Home/Count/Count';
 import { Why } from '../components/Home/Why/Why';
+import { About } from "../components/Home/About";
+import { Nav } from "../components/Home/navbar/Nav";
+import { Services } from "../components/Home/services/Services";
 import { MembershipFees } from '../components/Home/MembershipFees/MembershipFees';
 
 export const Home = () => {
   return (
     <Layout>
+      <Nav />
+      <Services />
+      <About />
       <MembershipFees />
       <Why />
       <Count />
